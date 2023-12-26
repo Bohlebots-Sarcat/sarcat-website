ac37aa9f6d60761769b1d0b816440f51afab0133
maupwastaken <78-maup@protonmail.com>
2023-12-26 16:25:04 +0100

    Update Svelte project layouts, migrate to static adapter and add firebase dependency
    The code changes reflect updates to Svelte's project layouts by adding text to headers in the main and hardware sections. The switch from the auto adapter to the static adapter in the Svelte config file has been made for better control over building configurations. Lastly, a new dependency has been added to the package.json for compatibility with Firebase.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@
     <section id="main">
         <Waves />
         <div class="relative">
-            <h1 class="absolute top-1/2 left-1/2 transform -translate-x-1/2 translate-y-28 text-secondary text-9xl font-jetbrains"></h1>
+            <h1 class="absolute top-1/2 left-1/2 transform -translate-x-1/2 translate-y-28 text-secondary text-9xl font-jetbrains">Sarcat</h1>
         </div>
         <div class="bg-primary h-screen flex justify-center items-center">
             <div class="rounded-lg p-10 gradient justify-center items-center flex h-screen w-screen">
@@ -28,6 +28,9 @@
         </div>
     </section>
     <section id="hardware">
+        <div class="relative">
+            <h1 class="absolute top-1/2 left-1/2 transform -translate-x-1/2 translate-y-28 text-secondary text-9xl font-jetbrains">Hardware</h1>
+        </div>
         <div class="h-screen bg-primary">
 
         </div>
--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,15 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'public',
+			assets: 'public',
+			fallback: '200.html' // Page to load when a prerendered route is not found
+		}),
 	},
-	preprocess: vitePreprocess()
+	preprocess: vitePreprocess() //Moved preprocess outside of the kit object
 };
 
 export default config;
